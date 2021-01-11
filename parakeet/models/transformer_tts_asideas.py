explicit broadcast, it does not fix it

parakeet/models/transformer_tts_asideas.py
@@ -325,8 +325,8 @@ class CNNPostNet(nn.Layer):
                 Conv1dBatchNorm(c_in, c_out, kernel_size, 
                                 weight_attr=I.XavierUniform(), 
                                 padding=padding,
-                                data_format="NLC"))
-        self.last_bn = nn.BatchNorm1D(d_output, data_format="NLC")
+                                data_format="NCL"))
+        self.last_bn = nn.BatchNorm1D(d_output, data_format="NCL")
     
     def forward(self, x):
         x_in = x
@@ -428,9 +428,9 @@ class TransformerTTS(nn.Layer):
         stop_logits = self.stop_conditioner(mel_intermediate)
         
         # cnn postnet
-        #mel_channel_first = paddle.transpose(mel_intermediate, [0, 2, 1])
-        mel_output = self.decoder_postnet(mel_intermediate)
-        #mel_output = paddle.transpose(mel_output, [0, 2, 1])
+        mel_channel_first = paddle.transpose(mel_intermediate, [0, 2, 1])
+        mel_output = self.decoder_postnet(mel_channel_first)
+        mel_output = paddle.transpose(mel_output, [0, 2, 1])
 
         return mel_output, mel_intermediate, cross_attention_weights, stop_logits
     
@@ -508,6 +508,7 @@ def weighted_mean(input, weight):
         Tensor: shape(1,), weighted mean tensor with the same dtype as input.
     """
     weight = paddle.cast(weight, input.dtype)
+    #weight_ = paddle.broadcast_to(weight, input.shape)
     return paddle.mean(input * weight)
 
 def masked_l1_loss(prediction, target, mask):

parakeet/modules/attention.py
@@ -27,6 +27,7 @@ def scaled_dot_product_attention(q, k, v, mask=None, dropout=0.0, training=True)
     scaled_logit = paddle.scale(qk, 1.0 / math.sqrt(d))
     
     if mask is not None:
+        mask = paddle.broadcast_to(mask, scaled_logit.shape)
         scaled_logit += paddle.scale((1.0 - mask), -1e9) # hard coded here
     
     attn_weights = F.softmax(scaled_logit, axis=-1)
@@ -60,7 +61,9 @@ def drop_head(x, drop_n_heads, training):
         np.random.shuffle(subarray)
     scale = float(num_heads) / (num_heads - drop_n_heads)
     mask = scale * np.reshape(mask, [batch_size, num_heads, 1, 1])
-    out = x * paddle.to_tensor(mask)
+    mask = paddle.to_tensor(mask)
+    mask = paddle.broadcast_to(mask, x.shape)
+    out = x * mask
     return out
 
 def _split_heads(x, num_heads):